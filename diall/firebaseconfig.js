c497759829250e7c16d89cfd8ddf7400576df2d5
VikashMediboina <58514852+VikashMediboina@users.noreply.github.com>
2024-01-10 09:51:10 -0500

    Update firebaseconfig.js

--- diall/firebaseconfig.js
+++ diall/firebaseconfig.js
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore"
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNqulX-KvXinbVcQyQ3xXHKdMT0BozIoI",
-  authDomain: "diall-22180.firebaseapp.com",
-  projectId: "diall-22180",
-  storageBucket: "diall-22180.appspot.com",
-  messagingSenderId: "251761625616",
-  appId: "1:251761625616:web:c0c646c30aca3c1dd55895",
-  measurementId: "G-VX6Q7GLTNX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const app=initializeApp(firebaseConfig);
 export const storage=getStorage(app)
